7e408b8f0dd738e8a6670da6e387501226dc5491
Jérémie Ferrand <jeremie.ferrand.pro@gmail.com>
2019-11-22 23:02:13 +0100

    database: use a connection pool to handle connections better
    The HikatiCP connection pool can speed-up the
    connection / disconnection process.
    The slf4 is used to get logs of the
    connection.

--- src/main/java/fr/uge/database/Database.java
+++ src/main/java/fr/uge/database/Database.java
@@ -1,38 +1,22 @@
 package fr.uge.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.*;
 import java.util.Objects;
 
-public class Database{
+public class Database {
 
-    /* TODO Faire en sorte que ça soit potable niveau connexion deco */
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
 
-    private final String url;
-
-    private Connection connection;
-    private Statement statement;
-    private boolean isConnected;
-
-    public Database(String url) throws ClassNotFoundException {
-        Class.forName("org.sqlite.JDBC");
-        this.url = Objects.requireNonNull(url);
+    static {
+        config.setJdbcUrl("jdbc:sqlite:src/main/resources/database/uge.db");
+        ds = new HikariDataSource(config);
     }
 
-    public void connect() throws SQLException {
-        if(isConnected){
-            close();
-        }
-        connection = DriverManager.getConnection(url);
-        statement = connection.createStatement();
-        isConnected = true;
-    }
-
-    public ResultSet query(String request) throws SQLException {
-        return statement.executeQuery(request);
-    }
-    public void close() throws SQLException {
-        statement.close();
-        connection.close();
-        isConnected = false;
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
     }
 }
